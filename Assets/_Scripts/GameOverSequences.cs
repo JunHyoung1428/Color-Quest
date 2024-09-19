968abd7b0c836f282b86e9a7b0d8e934d9ca34e3
Park JunHyoung <pj93251428@gmail.com>
2024-09-19 21:01:48 +0900

    feat : GameOverSequence(wip)

---
+++ Assets/_Scripts/GameOverSequences.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverSequences : MonoBehaviour
+{
+   
+    /// <summary>
+    /// 화면이 깨지는 연출
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator WithBreakScreen()
+    {
+        yield return null;
+    }
+
+
+    /// <summary>
+    /// 화면이 흐려지는(뿌옇게) 연출
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator WithSightBlur()
+    {
+        yield return null;
+    }
+
+    /// <summary>
+    /// 화면에  지지직 노이즈 끼는 연출
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator WithScreenNoise()
+    {
+        yield return null;
+    }
+}
